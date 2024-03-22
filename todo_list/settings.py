71d96cef22e31b077d9f876d90ae6da74eef3949
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-22 15:12:54 +0530

    .

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
