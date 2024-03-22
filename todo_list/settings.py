46920a8c23495f7120a148715a4c1ee63c6a32b2
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-22 14:42:55 +0530

    changed database to postgres

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fFePVVsLtzjkKVvpuRmmRCIoVUsAEtnp',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '45657',
     }
 }
 
